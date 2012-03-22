c16e2dd444d89e27472bfc29bd4ff6fca106ab53
kashiwasan <pc.kashiwasan@gmail.com>
2012-03-22 20:59:20 +0900

    fixed templates.

--- apps/mobile_frontend/modules/redirectTest/templates/indexSuccess.php
+++ apps/mobile_frontend/modules/redirectTest/templates/indexSuccess.php
@@ -3,8 +3,15 @@
 リダイレクトテスト<br />
 下記のフィールドのURLを確認し、「送信」ボタンを押してください。<br />
 <br />
-
-<input type="text" name="redirectUrl" value="<?php echo url_for('redirectTest/url', array('absolute' => true)) ?>" />
+【Cookieが対応していない端末】<br />
+* http:// から始まるURL (セッションIDが付与されない)<br />
+* symfony の内部URL ( member/search や @member_profile_mine → セッションIDが付与される) <br />
+<br />
+【Cookieに対応している端末】<br />
+* http:// から始まるURL (セッションIDが付与されない)<br />
+* symfony の内部URL ( member/search や @member_profile_mine → セッションIDが付与されない)<br />
+<br />
+<input type="text" name="redirectUrl" value="" />
 
 <br >
 
